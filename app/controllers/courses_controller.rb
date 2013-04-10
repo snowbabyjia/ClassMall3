7afc5d92d4abb761f5f46df53566577067174969
Jiajia Zhao <snowbabyjia@gmail.com>
2013-04-10 18:14:49 -0400

    Trying to make comment work...

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -14,11 +14,11 @@ class CoursesController < ApplicationController
   # GET /courses/1.json
   def show
     @course = Course.find(params[:id])
-
     respond_to do |format|
-      format.html # show.html.erb
+      format.html # new.html.erb
       format.json { render json: @course }
     end
+    #render :template => "comments/new"
   end
 
   # GET /courses/new
